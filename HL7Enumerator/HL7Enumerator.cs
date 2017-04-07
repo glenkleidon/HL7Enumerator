9b8a5e1fd576312d20f1f57c0b282c64a59b40c1
Glen Kleidon <glenk@galkam.com.au>
2017-04-07 19:54:41 +1000

    Initial Commit

---
+++ HL7Enumerator/HL7Enumerator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL7Enumerator
+{
+    
+    public class HL7Element : List<HL7Element> {
+        private string _separators;
+        private char _separator;
+        public string Separators { get { return _separators; } }
+        public char Separator { get { return _separator; } }
+        
+        private string value;
+        public string Value { get { return value; } }
+
+        public HL7Element(string data, char separator, string separators = "\n|~^&") {
+            _separators = separators;
+            _separator = separator;
+            var subElements = data.Split(separator);
+            var index = separators.IndexOf(separator);
+            if (index == separators.Length-1 || string.IsNullOrEmpty(data) ) {
+                this.value = data;
+            } else {
+                char nextchar = separators[index+1];
+                foreach (string s in subElements)
+                {
+                    this.Add(new HL7Element(s, nextchar, separators));
+                }
+                if (index==0) this.value = subElements[0];
+            }
+        }
+
+        public override string ToString()
+        {
+            if (this.Count() == 0) return value;
+            string result = "";
+            foreach (HL7Element HL7Element in this) {
+                return HL7Element.ToString() + Separator;
+            }
+            return result;
+        }
+    }
+
+    public class HL7Message  {
+
+        private HL7Element _segments;
+        public HL7Element Segments { get { return _segments; } }
+        public HL7Message(string mesg)
+        {
+            string separators='\n'+mesg.Substring(3,3)+mesg[7];
+            _segments = new HL7Element(mesg, '\n', separators);
+        }
+    }
+
+    
+}
---
+++ TestHL7Enumerator/TestHL7Enumerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestHL7Enumerator
+{
+    [TestClass]
+    public class UnitTestHL7Enumerator
+    {
+        private const string Example1 =
+              @"MSH|^~\&|CERNER||PriorityHealth||||ORU^R01|Q479004375T431430612|P|2.3|" + "\n" +
+              @"PID|||001677980||SMITH^CURTIS||19680219|M||||||||||929645156318|123456789|" + "\n" +
+              @"PD1||||1234567890^LAST^FIRST^M^^^^^NPI|" + "\n" +
+              @"OBR|1|341856649^HNAM_ORDERID|000002006326002362|648088^Basic Metabolic Panel|||20061122151600|||||||||1620^Hooker^Robert^L||||||20061122154733|||F|||||||||||20061122140000|" + "\n" +
+              @"OBX|1|NM|GLU^Glucose Lvl|59|mg/dL|65-99^65^99|L|||F|||20061122154733|" + "\n";
+
+        [TestMethod]
+        public void TestHL7Enumerator_create_intialises_a_instance()
+        {
+            HL7Enumerator.HL7Message msg = new HL7Enumerator.HL7Message(Example1);
+            Assert.IsFalse(string.IsNullOrEmpty(msg.Segments[0].ToString()));
+            Console.WriteLine(msg.Segments[0].ToString());
+        }
+    }
+}
